188eceb40858e3acade937a8ed11470a6fe997c0
AkshayVerma-Ipsator <akshay.verma@ipsator.com>
2026-05-06 03:54:03 +0530

    Updates

---
+++ _next/static/chunks/0-qb0w5-sb3x~.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,87814,t=>{t.v(c=>Promise.all(["static/chunks/06mu9aye~8cym.js"].map(c=>t.l(c))).then(()=>c(15090)))}]);
